25ff0b01fa9a7d861fd0eda244cd0c36b7068887
Josh Fernandez <jamiguelf396@gmail.com>
2018-10-31 16:30:59 -0700

    Flashcard T&D: Add code to the button

--- app/src/main/java/com/example/jam/joshfernandez_quizme/CreateFlashcardActivity.java
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/CreateFlashcardActivity.java
@@ -1,13 +1,32 @@
 package com.example.jam.joshfernandez_quizme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateFlashcardActivity extends AppCompatActivity {
+
+    private Button buttonAddNewFlashcard;
+    private EditText editTextFlashcardTerm, editTextFlashcardDefinition;
+    private String DEFAULT = "NULL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_flashcard);
+
+        buttonAddNewFlashcard = (Button)findViewById(R.id.buttonAddNewFlashcard);
+        editTextFlashcardTerm = (EditText)findViewById(R.id.editTextFlashcardTerm);
+        editTextFlashcardDefinition = (EditText)findViewById(R.id.editTextFlashcardDefinition);
+
+        buttonAddNewFlashcard.setOnClickListener((v)->{
+            Toast.makeText(this, "Going back to Display Flashcards Activity", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(CreateFlashcardActivity.this, DisplayFlashcardsActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
