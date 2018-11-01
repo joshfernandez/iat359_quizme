46355ec4b9982f68d664ab33fddde5768dd54c4c
Josh Fernandez <jamiguelf396@gmail.com>
2018-10-31 19:51:18 -0700

    Solo SQL Database: Modify DisplayFlashcards to align with RecyclerView

--- app/src/main/java/com/example/jam/joshfernandez_quizme/DisplayFlashcardsActivity.java
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/DisplayFlashcardsActivity.java
@@ -1,24 +1,33 @@
 package com.example.jam.joshfernandez_quizme;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class DisplayFlashcardsActivity extends AppCompatActivity {
+public class DisplayFlashcardsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private Button buttonCreateNewFlashcard, buttonDeleteFlashcardSet;
     private String DEFAULT = "NULL";
 
     private RecyclerView recyclerViewFlashcards;
-    private RecyclerView.Adapter myAdapter;
+    //private RecyclerView.Adapter myAdapter;
     private RecyclerView.LayoutManager myLayoutManager;
+
+    private MyDatabase db;
+    private MyAdapter myAdapter;
+    private MyHelper helper;
 
     ArrayList<String> courses = new ArrayList<String>(
             Arrays.asList("IAT100", "IAT102", "IAT201", "IAT235", "IAT265", "IAT312", "IAT 339", "IAT 359",
@@ -69,5 +78,43 @@ public class DisplayFlashcardsActivity extends AppCompatActivity {
         myLayoutManager = new LinearLayoutManager(this);
         recyclerViewFlashcards.setLayoutManager(myLayoutManager);
 
+
+        /*
+            PART C - Prepare SQLite Database.
+         */
+
+        db = new MyDatabase(this);
+        helper = new MyHelper(this);
+
+        Cursor cursor = db.getData();
+
+        int index1 = cursor.getColumnIndex(Constants.TERM);
+        int index2 = cursor.getColumnIndex(Constants.DEFINITION);
+
+        ArrayList<String> mArrayList = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            String flashcardTerm = cursor.getString(index1);
+            String flashcardDefinition = cursor.getString(index2);
+
+            String s = flashcardTerm + ", " + flashcardDefinition;
+            mArrayList.add(s);
+            cursor.moveToNext();
+        }
+
+        myAdapter = new MyAdapter(mArrayList, this);
+        recyclerViewFlashcards.setAdapter(myAdapter);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        LinearLayout clickedRow = (LinearLayout) view;
+        TextView flashcardTermTextView = (TextView) view.findViewById(R.id.flashcardTermEntry);
+        TextView flashcardDefinitionTextView = (TextView) view.findViewById(R.id.flashcardDefinitionEntry);
+
+        Toast.makeText(this,
+                "row " + (1+position) + ":  " + flashcardTermTextView.getText() + " --> " + flashcardDefinitionTextView.getText(),
+                Toast.LENGTH_LONG).show();
     }
 }
