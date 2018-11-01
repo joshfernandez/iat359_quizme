016de62ed0d9b2928f049d70d3b2c9d7cfae241c
Josh Fernandez <jamiguelf396@gmail.com>
2018-11-01 15:21:31 -0700

    Add dictionary functionality using Implicit Intents
    It's still not complete. I would like it if the definition is filled in the box.

--- app/src/main/java/com/example/jam/joshfernandez_quizme/CreateFlashcardActivity.java
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/CreateFlashcardActivity.java
@@ -1,6 +1,7 @@
 package com.example.jam.joshfernandez_quizme;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.Toast;
 
 public class CreateFlashcardActivity extends AppCompatActivity {
 
-    private Button buttonAddNewFlashcard;
+    private Button buttonAddNewFlashcard, buttonLookUp;
     private EditText editTextFlashcardTerm, editTextFlashcardDefinition;
     private String DEFAULT = "NULL";
 
@@ -19,8 +20,18 @@ public class CreateFlashcardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_flashcard);
 
         buttonAddNewFlashcard = (Button)findViewById(R.id.buttonAddNewFlashcard);
+        buttonLookUp = (Button)findViewById(R.id.buttonLookUp);
         editTextFlashcardTerm = (EditText)findViewById(R.id.editTextFlashcardTerm);
         editTextFlashcardDefinition = (EditText)findViewById(R.id.editTextFlashcardDefinition);
+
+        buttonLookUp.setOnClickListener((v)->{
+            String term_given = editTextFlashcardTerm.getText().toString();
+
+            Toast.makeText(this, "Proceed to the dictionary for " + term_given, Toast.LENGTH_SHORT).show();
+            Uri webpage = Uri.parse("http://www.merriam-webster.com/dictionary/" + term_given);
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
 
         buttonAddNewFlashcard.setOnClickListener((v)->{
             Toast.makeText(this, "Going back to Display Flashcards Activity", Toast.LENGTH_SHORT).show();
