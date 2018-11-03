c225e50fef3673d97e3e792f466ee3290cc589fb
Josh Fernandez <jamiguelf396@gmail.com>
2018-11-02 17:18:55 -0700

    Trial with Helmine: Move MyAdapter code to DisplayFlashcardsActivity

--- app/src/main/java/com/example/jam/joshfernandez_quizme/DisplayFlashcardsActivity.java
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/DisplayFlashcardsActivity.java
@@ -1,13 +1,17 @@
 package com.example.jam.joshfernandez_quizme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -16,6 +20,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static com.example.jam.joshfernandez_quizme.R.layout.row;
 
 public class DisplayFlashcardsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
@@ -154,4 +160,65 @@ public class DisplayFlashcardsActivity extends AppCompatActivity implements Adap
         }
 
     }
+}
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    public ArrayList<String> list;
+    Context appContext;
+
+    public MyAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.appContext = context;
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int ViewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(row, parent, false);
+        MyViewHolder viewHolder = new MyViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        String[]  results = (list.get(position).toString()).split(",");
+        holder.termTextView.setText(results[0]);
+        holder.definitionTextView.setText(results[1]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView termTextView;
+        public TextView definitionTextView;
+
+        public LinearLayout myLayout;
+
+        Context holderContext;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            myLayout = (LinearLayout) itemView;
+
+            termTextView = (TextView) itemView.findViewById(R.id.flashcardTermEntry);
+            definitionTextView = (TextView) itemView.findViewById(R.id.flashcardDefinitionEntry);
+
+            itemView.setOnClickListener(this);
+            holderContext = itemView.getContext();
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(holderContext,
+                    "You have clicked " + ((TextView)view.findViewById(R.id.flashcardTermEntry)).getText().toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
