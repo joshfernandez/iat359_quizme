d38dadcc832c8fc7297385aedf0e9fb5628b5228
Josh Fernandez <jamiguelf396@gmail.com>
2018-11-06 11:27:33 -0800

    Add HeadsUpActivity

---
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/HeadsUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.jam.joshfernandez_quizme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeadsUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heads_up);
+    }
+}
