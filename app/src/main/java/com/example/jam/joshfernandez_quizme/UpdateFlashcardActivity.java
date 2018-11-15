af9676933cbb5e4666232472afc2f352fb568133
Josh Fernandez <jamiguelf396@gmail.com>
2018-11-14 18:59:51 -0800

    Added button declaration + dictionary functionality

--- app/src/main/java/com/example/jam/joshfernandez_quizme/UpdateFlashcardActivity.java
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/UpdateFlashcardActivity.java
@@ -1,12 +1,16 @@
 package com.example.jam.joshfernandez_quizme;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UpdateFlashcardActivity extends AppCompatActivity {
 
+    private Button buttonUpdateFlashcard, buttonLookUp;
     private EditText editTextUpdateTerm, editTextUpdateDefinition;
 
     @Override
@@ -14,6 +18,8 @@ public class UpdateFlashcardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_flashcard);
 
+        buttonUpdateFlashcard = (Button) findViewById(R.id.buttonUpdateFlashcard);
+        buttonLookUp = (Button) findViewById(R.id.buttonLookUp);
         editTextUpdateTerm = (EditText) findViewById(R.id.editTextUpdateTerm);
         editTextUpdateDefinition = (EditText) findViewById(R.id.editTextUpdateDefinition);
 
@@ -21,5 +27,14 @@ public class UpdateFlashcardActivity extends AppCompatActivity {
 
         editTextUpdateTerm.setText(data.getStringExtra("Term"));
         editTextUpdateDefinition.setText(data.getStringExtra("Definition"));
+
+        buttonLookUp.setOnClickListener((v) -> {
+            String term_given = editTextUpdateTerm.getText().toString();
+
+            Toast.makeText(this, "Proceed to the dictionary for " + term_given, Toast.LENGTH_SHORT).show();
+            Uri webpage = Uri.parse("http://www.merriam-webster.com/dictionary/" + term_given);
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
     }
 }
