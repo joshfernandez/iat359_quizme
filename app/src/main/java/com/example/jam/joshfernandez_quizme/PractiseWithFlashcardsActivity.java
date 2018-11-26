dc7aae8b02c8b8d443c5c529e23697347589ff35
Josh Fernandez <jamiguelf396@gmail.com>
2018-11-25 21:18:36 -0800

    Initialize the PractiseWithFlashcardsActivity Java class

--- app/src/main/java/com/example/jam/joshfernandez_quizme/PractiseWithFlashcardsActivity.java
+++ app/src/main/java/com/example/jam/joshfernandez_quizme/PractiseWithFlashcardsActivity.java
@@ -2,12 +2,25 @@ package com.example.jam.joshfernandez_quizme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PractiseWithFlashcardsActivity extends AppCompatActivity {
+
+    private TextView textViewFlashcardPosition, textViewPractiseMain;
+    private Button buttonPrevious, buttonFlip, buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practise_with_flashcards);
+
+        textViewFlashcardPosition = (TextView) findViewById(R.id.textViewFlashcardPosition);
+        textViewPractiseMain = (TextView) findViewById(R.id.textViewPractiseMain);
+        buttonPrevious = (Button) findViewById(R.id.buttonPrevious);
+        buttonFlip = (Button) findViewById(R.id.buttonFlip);
+        buttonNext = (Button) findViewById(R.id.buttonNext);
     }
 }
